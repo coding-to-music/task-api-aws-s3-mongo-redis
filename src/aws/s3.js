c268dfd798a6b9519df71e30089b4b1e3817da0d
coding-to-music <connors.tom@gmail.com>
2022-12-11 06:00:57 +0000

    Build

--- src/aws/s3.js
+++ src/aws/s3.js
@@ -1,8 +1,8 @@
-const aws = require('aws-sdk');
+const aws = require("aws-sdk");
 aws.config.update({
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY
+  accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY,
 });
-const s3 = new aws.S3({ params: { Bucket: process.env.S3_BUCKET } });
+const s3 = new aws.S3({ params: { Bucket: process.env.APP_AWS_S3_BUCKET } });
 
 module.exports = s3;
